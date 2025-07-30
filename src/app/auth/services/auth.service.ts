a5249ee97994bb1a580a33cf7486d3dcd92003f9
Menna Ashraf <mennaalla1512@gmail.com>
2025-07-30 21:18:57 +0300

    remove unused import from auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { login } from '../interfaces/auth';
 
 @Injectable({
   providedIn: 'root',
